e0652a6d6d0b3d3fb3fa093ac9766ece8ff716d7
neoscraper <thomoh777@gmail.com>
2022-03-16 01:03:58 -0600

    Basic people controller
    Added base people controller functionality

--- WebAPI/WebAPI/Controllers/PeopleController.cs
+++ WebAPI/WebAPI/Controllers/PeopleController.cs
@@ -4,26 +4,36 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
     public class PeopleController : ApiController
     {
-        // GET: api/People
-        public IEnumerable<string> Get()
+        List<Person> people = new List<Person>();
+        public PeopleController()
         {
-            return new string[] { "value1", "value2" };
+            people.Add(new Person { FirstName = "Mario", LastName = "Mario", ID = 1 });
+            people.Add(new Person { FirstName = "Sakata", LastName = "Gintoki", ID = 2 });
+            people.Add(new Person { FirstName = "Bruce", LastName = "Wayne", ID = 3 });
+        }
+
+        // GET: api/People
+        public IEnumerable<Person> Get()
+        {
+            return people;
         }
 
         // GET: api/People/5
-        public string Get(int id)
+        public Person Get(int id)
         {
-            return "value";
+            return people.Where(x => x.ID == id).FirstOrDefault();
         }
 
         // POST: api/People
-        public void Post([FromBody]string value)
+        public void Post(Person person)
         {
+            people.Add(person);
         }
 
         // DELETE: api/People/5
